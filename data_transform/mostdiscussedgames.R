c069b992ad3a51acbfd988f1fd74027c383932dd
gb <glumbongo93@gmail.com>
2020-11-26 16:52:29 +0000

    dataframe ready for use

--- data_transform/mostdiscussedgames.R
+++ data_transform/mostdiscussedgames.R
@@ -4,7 +4,10 @@ library(tidytext)
 playstation_english <-  read_csv("playstation_tweets_english.csv", col_names = TRUE)
 xbox_x_english <-  read_csv("xboxseriesx_tweets_english.csv", col_names = TRUE)
 
+playstation_english <- playstation_english %>% select(text) %>% mutate(platform = "Playstation")
+xbox_x_english <- xbox_x_english %>% select(text) %>% mutate(platform = "Xbox")
 
+playstation_xbox_text <- rbind(playstation_english, xbox_x_english)
 
 
 game_name <- c("Astro's Playroom","Demon's Souls",
@@ -34,7 +37,7 @@ platform <- c("Playstation 5","Playstation 5","Playstation 5",
               "Multiplatform", "Multiplatform", "Xbox Series X")
 
 keyword <- c("astro","Souls","spiderman","sackboy","bugsnax","godfall",
-             "pathless","forza","gears","tactics","grounded","ori","bright memory",
+             "pathless","forza","gears","tactics","grounded","wisps","bright memory",
              "daylight","enlisted","evergate","manifold","tetris",
              "touryst","falconeer","thunder","grace", "halo", "valhalla",
              "borderlands", "cod", "cyberpunk", "destiny", "devil", "dirt",
@@ -49,41 +52,40 @@ genre <- c("Platformer","Action RPG","Action-Adventure","Platformer","Adventure"
            "Racing", "Fighting", "Third-person Shooter", "Musical", "Action-Adventure", "Fighting",
            "Sport", "Horror", "Sim", "Hack and Slash", "Action-Adventure", "RPG", "n/a")
 
-platform <- factor(platform)
-genre <- factor(genre)
-
-games <- data.frame(name = game_name, genre = genre, keyword = keyword, platform = platform)
 
 
+games <- data.frame(name = game_name, genre = genre, keyword = keyword, platform = platform, stringsAsFactors = FALSE)
 
 
-games_by_volume <- data.frame()
-
+#returns the filtered tweets based on games in a dataframe
 discussed_games_prep <- function(dataframe, regex, game_name, output_frame){
   
   dataframe$stripped_text <- gsub("http.*","", dataframe$text) #strip the text of links etc etc
   dataframe$stripped_text <- gsub("https.*","", dataframe$stripped_text)
   dataframe$stripped_text <- gsub("amp","", dataframe$stripped_text)
   
-  intermediate1 <- dataframe %>% select(stripped_text) %>% mutate(tweet_number = row_number()) #add tweet numbers to keep track
+  intermediate1 <- dataframe %>% select(stripped_text, platform) %>% mutate(tweet_number = row_number()) #add tweet numbers to keep track
   
   #isolate the tweets containing the keyword from the dataframe, unnest the text into singular words and filter the stop words. 
   intermediate2 <- intermediate1 %>% filter(grepl(regex, stripped_text, ignore.case = TRUE)) %>% mutate(game = game_name)
-  output_frame <- rbind(output_frame, intermediate2)
-    
 }
 
 
-discussed_games_prep(xbox_x_english, "souls","Dark Souls", games_by_volume)
-
-
+#search for tweets concerning all 41 games in the dataframe and return each as a list 
+games_by_volume_list <- list()
 for (i in 1:length(games$name)) {
-  discussed_games_prep(xbox_x_english, games$keyword[i], games$name[i])
+  games_by_volume_list[[i]] <- discussed_games_prep(playstation_xbox_text, games$keyword[i], games$name[i])
 }
 
+launch_games_tweets <- do.call(rbind, games_by_volume_list)
+launch_games_tweets <- launch_games_tweets %>% group_by(game) %>%
+  mutate(tweet_count = n())
+
+#dataframe now ready to use for further analysis 
 
 
-mapply(discussed_games_prep, xbox_x_english, keyword, game_name)
 
-
-playstation_darksouls <- discussed_games_prep(playstation_english, "souls", "Demon's Souls")
+launch_games_tweets %>% ggplot(aes(x = reorder(game, -tweet_count))) +
+  geom_bar(stat="count") +
+  theme(axis.text.x = element_text(angle = 90, size = 7, hjust = 1, vjust = 0.2)
+  )
