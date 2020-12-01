9af693dd6508df230e6f2b03e84cfbf9e3923b97
gb <glumbongo93@gmail.com>
2020-12-01 18:23:32 +0000

    added sentiment analysis for top10 games. still need to do multiple variable ttest

--- data_transform/mostdiscussedgames.R
+++ data_transform/mostdiscussedgames.R
@@ -1,13 +1,14 @@
 library(tidyverse)
 library(tidytext)
 
-playstation_english <-  read_csv("playstation_tweets_english.csv", col_names = TRUE)
-xbox_x_english <-  read_csv("xboxseriesx_tweets_english.csv", col_names = TRUE)
+setwd("university/math513/coursework/presentation/csv_files")
 
-playstation_english <- playstation_english %>% select(text) %>% mutate(platform = "Playstation")
-xbox_x_english <- xbox_x_english %>% select(text) %>% mutate(platform = "Xbox")
 
-playstation_xbox_text <- rbind(playstation_english, xbox_x_english)
+
+mg_playstation_english <- playstation_english %>% select(text, created_at) %>% mutate(platform = "Playstation")
+mg_xbox_x_english <- xbox_x_english %>% select(text, created_at) %>% mutate(platform = "Xbox")
+
+mg_playstation_xbox_text <- rbind(mg_playstation_english, mg_xbox_x_english)
 
 
 game_name <- c("Astro's Playroom","Demon's Souls",
@@ -64,7 +65,7 @@ discussed_games_prep <- function(dataframe, regex, game_name, output_frame){
   dataframe$stripped_text <- gsub("https.*","", dataframe$stripped_text)
   dataframe$stripped_text <- gsub("amp","", dataframe$stripped_text)
   
-  intermediate1 <- dataframe %>% select(stripped_text, platform) %>% mutate(tweet_number = row_number()) #add tweet numbers to keep track
+  intermediate1 <- dataframe %>% select(stripped_text, platform, created_at) %>% mutate(tweet_number = row_number()) #add tweet numbers to keep track
   
   #isolate the tweets containing the keyword from the dataframe, unnest the text into singular words and filter the stop words. 
   intermediate2 <- intermediate1 %>% filter(grepl(regex, stripped_text, ignore.case = TRUE)) %>% mutate(game = game_name)
@@ -74,7 +75,7 @@ discussed_games_prep <- function(dataframe, regex, game_name, output_frame){
 #search for tweets concerning all 41 games in the dataframe and return each as a list 
 games_by_volume_list <- list()
 for (i in 1:length(games$name)) {
-  games_by_volume_list[[i]] <- discussed_games_prep(playstation_xbox_text, games$keyword[i], games$name[i])
+  games_by_volume_list[[i]] <- discussed_games_prep(mg_playstation_xbox_text, games$keyword[i], games$name[i])
 }
 
 launch_games_tweets <- do.call(rbind, games_by_volume_list)
@@ -84,8 +85,64 @@ launch_games_tweets <- launch_games_tweets %>% group_by(game) %>%
 #dataframe now ready to use for further analysis 
 
 
-
-launch_games_tweets %>% ggplot(aes(x = reorder(game, -tweet_count))) +
+#most discussed games within the entire dataset, ordered from ost to least
+launch_games_tweets %>% 
+  ggplot(aes(x = reorder(game, -tweet_count))) +
   geom_bar(stat="count") +
-  theme(axis.text.x = element_text(angle = 90, size = 7, hjust = 1, vjust = 0.2)
-  )
+  theme(axis.text.x = element_text(angle = 90, size = 7, hjust = 1, vjust = 0.2))
+
+
+
+
+#need to load in launch_games_tweets
+
+#establish the top 10 discussed games by n- of tweets
+top10_filter <- launch_games_tweets %>% count(game) %>% arrange(desc(n)) %>% head(10)
+
+#filter tweet list to just have the top 10 games 
+top10_tweets <- launch_games_tweets %>% filter(game %in% top10_filter$game)
+console_my_stop_words <- data.frame(word = c("playstation","ps5","PS5","xbox","gaming","games","game"))
+
+#unnest text to analyse 
+top10games_unnested <- top10_tweets %>% select(stripped_text, tweet_number, created_at) %>% unnest_tokens(word, stripped_text)
+
+#introduce stop words 
+top10games_sentiment <- top10games_unnested %>% anti_join(stop_words, by = c("word" = "word"))
+top10games_sentiment_2 <- top10games_sentiment %>%  anti_join(console_my_stop_words, by = c("word" = "word"))
+
+#using binary sentiment library
+game_sentiment <- top10games_sentiment_2 %>%
+    inner_join(get_sentiments("bing")) %>%
+    mutate(sentiment = ifelse(word == "hype", "positive", sentiment))
+
+#overall score per tweet with time included for more detailed analysis 
+game_sentiment_score <- game_sentiment %>% count(tweet_number, created_at, sentiment) %>%
+  spread(sentiment, n, fill = 0) %>% mutate(score = positive - negative)
+
+
+#mean sentiment for each game
+mean_sentiment_game <- game_sentiment_score %>% group_by(game) %>% summarise(avg_sentiment = mean(score))
+#cyberpunk held down by delay news that came out october 27th
+
+#multi variable t-test for the sentiments of each game 
+
+
+#sentiment of games every 6 hours
+game_sentiment_6_hour <- game_sentiment_score %>% mutate(six_hour_intervals = cut.POSIXt(created_at, breaks = "6 hours")) %>% 
+  group_by(game, six_hour_intervals) %>% summarise(avg_sentiment = mean(score))
+
+game_sentiment_6_hour  %>% group_by(game) %>% ggplot(aes(x = six_hour_intervals, y = avg_sentiment, color = game,
+                                                        group = game)) +
+  geom_point() +
+  geom_line() +
+  theme(axis.text.x = element_text(angle = 90, size = 7, hjust = 1, vjust = 0.2))
+
+#sentiment of games every day
+game_sentiment_one_day <- game_sentiment_score %>% mutate(one_day_intervals = cut.POSIXt(created_at, breaks = "1 day")) %>% 
+  group_by(game, one_day_intervals) %>% summarise(avg_sentiment = mean(score))
+
+game_sentiment_one_day  %>% group_by(game) %>% ggplot(aes(x = one_day_intervals, y = avg_sentiment, color = game,
+                                                         group = game)) +
+  geom_point() +
+  geom_line() +
+  theme(axis.text.x = element_text(angle = 90, size = 7, hjust = 1, vjust = 0.2))
